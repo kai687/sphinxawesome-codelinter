49f9729f8085d1c291ea44566f7adae849a6f962
Kai Welke <17420240+kai687@users.noreply.github.com>
2020-01-09 14:36:36 +0100

    further unit tests implemented

--- tests/example/test-root/index.rst
+++ tests/example/test-root/index.rst
@@ -1,5 +1,12 @@
 Test the Code Linter Extension
 ==============================
 
-
 Just a normal paragraph.
+
+.. code-block:: json
+
+   { "json" : "data" }
+
+.. code-block::
+
+   $ don't lint this
--- tests/test_sphinxawesome_codelinter.py
+++ tests/test_sphinxawesome_codelinter.py
@@ -2,7 +2,9 @@ import os
 import pytest
 from sphinxawesome.codelinter import __version__
 
+
 def test_version():
+    '''test that the version we expect is indeed here.'''
     assert __version__ == '0.1.0'
 
 
@@ -13,7 +15,7 @@ def test_rootdir_fixture(rootdir):
     tell pytest/sphinx that I do not want an extra nesting level here.
     '''
 
-    conf_file = rootdir / 'test-root' /  'conf.py'
+    conf_file = rootdir / 'test-root' / 'conf.py'
     index_file = rootdir / 'test-root' / 'index.rst'
     assert 'example' in rootdir
     assert conf_file.exists()
@@ -32,17 +34,55 @@ def test_basic_config(app):
     assert not os.listdir(app.outdir)
 
 
-#  @pytest.mark.sphinx('dummy', srcdir='example',
-                    #  confoverrides={'extensions': ['sphinxawesome-codelinter']})
-#  def test_extension_enabled(app):
-    #  '''
-    #  We want to test enabling the extension, but not configuring it.
-    #  '''
+@pytest.mark.sphinx('dummy', srcdir='example',
+                    confoverrides={'extensions': ['sphinxawesome.codelinter']})
+def test_extension_enabled(app):
+    '''
+    Test enabling the extension without configuring it to run.
+    '''
 
-    #  app.builder.build_all()
-    #  assert app.outdir.exists()
-    #  assert not os.listdir(app.outdir)
+    app.builder.build_all()
+    assert app.outdir.exists()
+    assert not os.listdir(app.outdir)
+    assert 'codelinter_languages' in app.config
 
-# test that enabling the extension leads to a successful compilation
-# test that enabling and configuring the extension leads to a successful run
-# test that configuring but not enabling the extension throws an error
+
+@pytest.mark.sphinx('codelinter', srcdir='example',
+                    confoverrides={'extensions': ['sphinxawesome.codelinter']})
+def test_codelinter_empty(app):
+    '''
+    Test the codelinter builder without any configuration.
+    '''
+
+    app.builder.build_all()
+    assert app.outdir.exists()
+    assert not os.listdir(app.outdir)
+    assert 'codelinter_languages' in app.config
+
+
+@pytest.mark.sphinx('dummy', srcdir='example',
+                    confoverrides={
+                        'extensions': ['sphinxawesome.codelinter'],
+                        'codelinter_languages': {'json': 'python -m json.tool'}
+                    })
+def test_dummy_configured(app):
+    '''
+    Test normal builder with configured codelinter_languages dict.
+    '''
+    app.builder.build_all()
+    assert app.outdir.exists()
+    assert not os.listdir(app.outdir)
+
+
+@pytest.mark.sphinx('codelinter', srcdir='example',
+                    confoverrides={
+                        'extensions': ['sphinxawesome.codelinter'],
+                        'codelinter_languages': {'json': 'python -m json.tool'}
+                    })
+def test_codelinter_configured(app):
+    '''
+    Test codelinter builder.
+    '''
+    app.builder.build_all()
+    assert app.outdir.exists()
+    assert not os.listdir(app.outdir)
