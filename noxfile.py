8804a69aa5086142f548d4250a909d3a618d1895
Kai Welke <kai687@pm.me>
2020-06-11 21:19:21 +0200

    test against multiple versions of Sphinx

--- noxfile.py
+++ noxfile.py
@@ -26,12 +26,23 @@ def install_constrained_version(session: Session, *args: str, **kwargs: Any) -> 
 
 
 @nox.session(python=["3.6", "3.7", "3.8"])
-def tests(session: Session) -> None:
+@nox.parametrize("sphinx", ["2.*", "3.*"])
+def tests(session: Session, sphinx: str) -> None:
     """Run unit tests."""
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_constrained_version(session, "coverage[toml]", "pytest", "pytest-cov")
+    # override the default Sphinx version (which will be 3.x)
+    session.run("poetry", "run", "pip", "install", f"sphinx=={sphinx}", external=True)
     session.run("pytest", *args)
+    session.run(
+        "poetry",
+        "run",
+        "python3",
+        "-c",
+        "import sphinx; print(sphinx.__version__)",
+        external=True,
+    )
 
 
 @nox.session(python=["3.6", "3.7", "3.8"])
